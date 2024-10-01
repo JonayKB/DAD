cf2ea05710d975512174e54e9c78bd6c2d0987fa
JonayKB <jonaykb@gmail.com>
2024-10-01 23:08:39 +0100

    feat(LolWiever): Temrinada

--- LolViewer/scripts/index.js
+++ LolViewer/scripts/index.js
@@ -1,28 +1,61 @@
 const DOM = {
     container: document.getElementById('container'),
-}
+};
 
 async function getChampions() {
+    // Fetch the list of champions
     const response = await fetch('https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/champion.json');
     const data = await response.json();
-    Object.values(data.data).forEach((champion) => {
-        let champions = [];
-        fetch(`https://ddragon.leagueoflegends.com/cdn/14.19.1/data/en_US/champion/${champion.id}.json`)
-            .then(res => res.json())
-            .then(
-                data => { champions.push(data.data) })
-        return champions;
 
+    let champions = [];
+
+
+    for (let champion of Object.values(data.data)) {
+        const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/champion/${champion.id}.json`);
+        const detailedData = await res.json();
+        console.log(Object.values(detailedData.data)[0]);
+        champions.push(Object.values(detailedData.data)[0]);
     }
-    );
+
+    return champions;
 }
 
-function showChampions() {
+async function showChampions() {
 
-};
+    const champions = await getChampions();
+
+
+    champions.forEach((champion) => {
+        const championElement = document.createElement('div');
+        championElement.classList.add('champion');
+
+
+        championElement.innerHTML = `
+    <div class="image-container">
+        <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg" alt="${champion.name}" class="loading">
+        <h2>${champion.name}</h2>
+        <div class="abilities">
+            <div class="ability-container">
+                <img src="https://ddragon.leagueoflegends.com/cdn/14.19.1/img/passive/${champion.passive.image.full}" alt ="${champion.passive.name}" class="ability"/>
+                <span class="tooltip">${champion.passive.name}</span>
+            </div>
+            ${champion.spells.map(spell => `
+            <div class="ability-container">
+                <img src="https://ddragon.leagueoflegends.com/cdn/14.19.1/img/spell/${spell.image.full}" alt="${spell.id}" class="ability"/>
+                <span class="tooltip">${spell.name}</span>
+            </div>`).join('')}
+        </div>
+    </div>
+    <p>${champion.title}</p>
+`;
+
+
+
+        DOM.container.appendChild(championElement);
+    });
+}
 
 showChampions();
 
-/**
- * 
- */
+
+showChampions();
